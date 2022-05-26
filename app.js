ba2895974cd8612b7b5b05fca1d3c363ee1d59d1
asmaasifchippa786 <asmaasifchippa786@gmail.com>
2022-05-26 13:48:02 +0500

    1st

---
+++ app.js
@@ -0,0 +1,21 @@
+var first,two,three;
+
+var Namefirst;
+
+var 1Name ;
+
+var nameali;
+
+var 124;
+
+var name/ali;
+
+var nameIsAli;
+
+var name1Hk;
+
+var nameInk;
+
+var nameOfWife;
+
+var yourName;
